bb7cc39282bad35efc3245dc6e35ec06134f573a
Dakshita Mishra <43453433+dakshitamishra@users.noreply.github.com>
2023-02-19 13:35:59 +0530

    Add files via upload

---
+++ NumericalOperators.py
@@ -0,0 +1,23 @@
+
+#addition
+a=5
+b=3
+print(a+b)
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#float division
+print(a/b)
+
+#integer division
+print(a//b)
+
+#modulus
+print(a%b)
+
+#power calculation
+print(a**b)
